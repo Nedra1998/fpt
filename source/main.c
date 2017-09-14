b53a342cbf290c97b0baaa290e7e49cf7c89ba28
Arden Rasmussen <ardenisthebest@gmail.com>
2017-09-13 19:15:34 -0700

    Testing os specific code

--- source/main.c
+++ source/main.c
@@ -1,6 +1,20 @@
 #include "fpt.h"
 
+#include <stdio.h>
+
 int main(int argc, char const *argv[]) {
+#ifdef __linux__
+  printf("This is linux");
+#endif
+#if _WIN32
+  printf("This is windows");
+#endif
+#if __APPLE__
+  printf("This is osx");
+#endif
+#if __unix__
+  printf("This is some unix");
+#endif
   /* code */
   return 0;
 }
