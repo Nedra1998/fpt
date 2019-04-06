cd955a4fa4c28b125d1245e109717d22c1d42be7
Arden Rasmussen <ardenisthebest@gmail.com>
2019-04-06 12:04:14 -0700

    Added graphics core

--- example/main.c
+++ example/main.c
@@ -1,11 +1,26 @@
 #include "FPT.h"
 
-#include <stdio.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[])
 {
   unsigned major, minor;
   G_version(&major, &minor);
   printf("V%u.%u", major, minor);
+  G_init_graphics(500, 500);
+  G_hex(0xff00ff);
+  /* sleep(1); */
+  for(int i = 0; i < 50; ++i){
+    int x = rand() % 500;
+    int y = rand() % 500;
+    int r = rand() % 10;
+    if(!G_fill_circle(x, y, r)){
+      printf("ERR\n");
+    }
+  }
+  G_display_image();
+  G_save_image_to_file("test.xwd");
+  sleep(2);
+  G_term_graphics();
   return 0;
 }
--- src/FPT.h
+++ src/FPT.h
@@ -1,6 +1,14 @@
 #ifndef FPT_H_
 #define FPT_H_
 
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+#include "graphics.h"
+
 #define FPT_VERSION_MAJOR 2
 #define FPT_VERSION_MINOR 0
 
